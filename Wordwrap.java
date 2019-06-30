96eb3a6a17ec48de223be30f5703c321b8980772
sripa <sripa@10.0.0.25>
2019-06-30 14:39:30 -0600

    Validation checks for Invalid Text in Wordwrap method.

--- Wordwrap.java
+++ Wordwrap.java
@@ -10,4 +10,10 @@ public class Wordwrap {
 	public static void main(String[] args) {
 
 	}
+	public static String wrap(String text, int maxLineLength) {
+
+		// checking if the input passed is null or empty. If the input passed is empty/null we are throwing IllegalArgument Exception
+		if (text == null || text.equals("")) {
+			throw new IllegalArgumentException("String is empty. There is nothing to word wrap");
+		}
 }
