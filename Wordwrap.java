5e15183cd5faacac0601cec3a8fea649fb73e896
sripa <sripa@10.0.0.25>
2019-06-30 14:36:34 -0600

    Invalid Text test methods.

--- Wordwrap.java
+++ Wordwrap.java
@@ -1,1 +1,13 @@
 
+/***
+ * Class for word wrapping a given String
+ * 
+ * @author Sri Pallavi Atkuri
+ *
+ */
+public class Wordwrap {
+
+	public static void main(String[] args) {
+
+	}
+}
--- WordwrapTest.java
+++ WordwrapTest.java
@@ -1,1 +1,19 @@
+import org.junit.jupiter.api.Test;
 
+import junit.framework.Assert;
+
+class WordwrapTest {
+	// Invalid input tests
+		@Test
+		void testEmptyText() {
+
+			IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> Wordwrap.wrap("", 0));
+			assertEquals("String is empty. There is nothing to word wrap", exception.getMessage());
+		}
+
+		@Test
+		void testNullText() {
+			IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> Wordwrap.wrap(null, 0));
+			assertEquals("String is empty. There is nothing to word wrap", exception.getMessage());
+		}
+}
