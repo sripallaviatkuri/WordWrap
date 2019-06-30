978d7f20782900ff39ddb5b15a84c0476342ce38
sripa <sripa@10.0.0.25>
2019-06-30 14:44:03 -0600

    Invalid maxLineLength test cases

--- WordwrapTest.java
+++ WordwrapTest.java
@@ -4,16 +4,32 @@ import junit.framework.Assert;
 
 class WordwrapTest {
 	// Invalid input tests
-		@Test
-		void testEmptyText() {
+	@Test
+	void testEmptyText() {
 
-			IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> Wordwrap.wrap("", 0));
-			assertEquals("String is empty. There is nothing to word wrap", exception.getMessage());
-		}
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> Wordwrap.wrap("", 0));
+		assertEquals("String is empty. There is nothing to word wrap", exception.getMessage());
+	}
 
-		@Test
-		void testNullText() {
-			IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> Wordwrap.wrap(null, 0));
-			assertEquals("String is empty. There is nothing to word wrap", exception.getMessage());
-		}
+	@Test
+	void testNullText() {
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> Wordwrap.wrap(null, 0));
+		assertEquals("String is empty. There is nothing to word wrap", exception.getMessage());
+	}
+
+	@Test
+	void testZeroMaxLineLengthException() {
+
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+				() -> Wordwrap.wrap("hello", 0));
+		assertEquals("invalid LineLength", exception.getMessage());
+
+	}
+
+	@Test
+	void testNegativeMaxLineLengthException() {
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+				() -> Wordwrap.wrap("hello", 0));
+		assertEquals("invalid LineLength", exception.getMessage());
+	}
 }
