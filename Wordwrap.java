2ad150698444244340f8a562d412757f669c642b
sripa <sripa@10.0.0.25>
2019-06-30 14:47:45 -0600

    Throwing IllegaArgumentExcetion in case of Invalid MaxLineLength argument

--- Wordwrap.java
+++ Wordwrap.java
@@ -8,12 +8,22 @@
 public class Wordwrap {
 
 	public static void main(String[] args) {
-
+		System.out.println(wrap("Hello Sri. How I am doing? How is everything", 4));
 	}
+
+	
+
 	public static String wrap(String text, int maxLineLength) {
 
 		// checking if the input passed is null or empty. If the input passed is empty/null we are throwing IllegalArgument Exception
 		if (text == null || text.equals("")) {
 			throw new IllegalArgumentException("String is empty. There is nothing to word wrap");
 		}
+		// checking if maxLineLength input is <=0. If it is then we are throwing IllegalArgument Exception
+				if (maxLineLength <= 0) {
+
+					throw new IllegalArgumentException("invalid LineLength");
+
+				}
+
 }
